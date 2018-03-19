3f876c4eef9b442a35e7e6b02c3fe699d1f64c8b
Tup Luisking <ramon.luisking14@gmail.com>
2018-03-19 14:30:02 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//$("#nav").localScroll();
+
+var navHeight = 110;
+
+//function navTo(element) {
+//    
+//    clearInterval(this);
+//    
+//    console.log(element);
+//    
+//    $('html, body').animate({
+//        scrollTop: $(element.getAttribute("href")).offset().top - navHeight
+//    }, 750);
+//}
