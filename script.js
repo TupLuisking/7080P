efc4d372d1c0a99515ae8b328b3d49e9c0de7939
Tup Luisking <ramon.luisking14@gmail.com>
2018-03-20 23:56:48 +0800

    added slideshow; 3 minifilms; calendar + events

--- script.js
+++ script.js
@@ -12,15 +12,54 @@ $(window).scroll(function(){
     console.log(wScroll);
 });
 
-//var navHeight = 110;
+var navHeight = 110;
 
-//function navTo(element) {
-//    
-//    clearInterval(this);
-//    
-//    console.log(element);
-//    
-//    $('html, body').animate({
-//        scrollTop: $(element.getAttribute("href")).offset().top - navHeight
-//    }, 750);
-//}
+function navTo(element) {
+    
+    clearInterval(this);
+    
+    console.log(element);
+    
+    $('html, body').animate({
+        scrollTop: $(element.getAttribute("href")).offset().top - navHeight
+    }, 750);
+}
+
+var slideIndex = 1;
+showSlide(slideIndex);
+
+function plusSlide(n) {
+    
+    showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+    
+    console.log("Changing slide");
+    
+    var i;
+    var x = document.getElementsByClassName("slideshow");
+    
+    if(n > x.length)
+        slideIndex = 1;
+    
+    if(n < 1)
+        slideIndex = x.length;
+    
+    for(i = 0; i < x.length; i++)
+        x[i].style.display = "none";
+    
+    x[slideIndex-1].style.display = "block";
+}
+
+function showDayInfo() {
+    
+    var dayInfo = document.getElementById("dayinfo");
+    dayInfo.style.maxWidth = "100%";
+}
+
+function hideDayInfo() {
+    
+    var dayInfo = document.getElementById("dayinfo");
+    dayInfo.style.maxWidth = "0";
+}
